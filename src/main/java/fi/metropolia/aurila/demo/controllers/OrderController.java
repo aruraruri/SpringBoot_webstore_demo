7ceab02e0d65ddde6c85ad94fa181ad372e9bea3
aruraruri <aurilait@gmail.com>
2026-02-27 13:50:42 +0200

    Add createOrder method to OrderController for order creation

--- src/main/java/fi/metropolia/aurila/demo/controllers/OrderController.java
+++ src/main/java/fi/metropolia/aurila/demo/controllers/OrderController.java
@@ -3,10 +3,7 @@ package fi.metropolia.aurila.demo.controllers;
 import fi.metropolia.aurila.demo.entity.Order;
 import fi.metropolia.aurila.demo.repositories.OrderRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -22,5 +19,10 @@ public class OrderController {
     @GetMapping("/{id}")
     public ResponseEntity<Order> getOrderById(@PathVariable final Integer id) {
         return repository.findById(id).map(order -> ResponseEntity.ok(order)).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody final Order order) {
+        return ResponseEntity.ok(repository.save(order));
     }
 }
